c0ce2d492c6362428eefc01b57604d10ebced49f
Ruksana0138 <81854065+Ruksana0138@users.noreply.github.com>
2025-10-05 18:30:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const server = createServer(app)
 const io = new Server(server,           
     {cors:{
-        origin:"https://socket-io-chat-app-frontend.vercel.app",    //cors is security measure of backend.HERE ONLY MENTIONED frontend's request is allowed to backend
+        origin:"https://chat-app-sandy-psi-16.vercel.app/",    //cors is security measure of backend.HERE ONLY MENTIONED frontend's request is allowed to backend
         methods:["GET","POST"]
     }}
 )
